1eccd735eb707034e88d71216c957aa4885beb51
kongyl <kongyl@126.com>
2020-05-04 17:07:23 +0800

    temp

--- src/main/java/me/kongyl/template/controller/MainController.java
+++ src/main/java/me/kongyl/template/controller/MainController.java
@@ -1,14 +1,26 @@
 package me.kongyl.template.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import me.kongyl.template.base.BaseController;
+import me.kongyl.template.util.CodeUtils;
 
 @Controller
-public class MainController {
+public class MainController extends BaseController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "index";
+    }
+    
+    @ResponseBody
+    @PostMapping("/login")
+    public String login(String username, String password, Boolean isRemember) {
+        String token = CodeUtils.uuid();
+        return token;
     }
     
 }
---
+++ src/main/java/me/kongyl/template/util/CodeUtils.java
@@ -0,0 +1,15 @@
+package me.kongyl.template.util;
+
+import java.util.UUID;
+
+public class CodeUtils {
+
+    private CodeUtils() {}
+    
+    public static String uuid() {
+        return UUID.randomUUID().toString();
+    }
+    
+    public 
+    
+}
